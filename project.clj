f01e00ae0334c9a799ef268bd4fb29b00b041427
Alexander Oloo <alekcz@gmail.com>
2020-04-22 19:24:09 +0200

    update deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [ [org.clojure/clojure "1.10.0"]
-                  [alekcz/konserve-fire "0.0.9-SNAPSHOT"]
+                  [alekcz/konserve-fire "0.1.0-SNAPSHOT"]
                   [io.replikativ/datahike "0.2.1"]]
   :repl-options {:init-ns datahike-firebase.core}
   :plugins [[lein-cloverage "1.1.2"]]
