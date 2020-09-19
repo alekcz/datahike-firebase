f5de4a5f89bc9857c25083d287f1583928446ec9
Alexander Oloo <alekcz@gmail.com>
2020-09-19 18:16:30 +0200

    update deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/alekcz/datahike-firebase"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [ [org.clojure/clojure "1.10.0"]
-                  [alekcz/konserve-fire "0.3.0-20200714.163253-5"]
-                  [io.replikativ/datahike "0.3.2-20200713.140023-3"]]
+  :dependencies [ [org.clojure/clojure "1.10.1" :scope "provided"]
+                  [alekcz/konserve-fire "0.3.0-20200919.161446-8"]
+                  [io.replikativ/datahike "0.3.2-20200918.094908-15" :exclusions [io.replikativ/superv.async]]]
   :aot :all                
   :repl-options {:init-ns datahike-firebase.core}
-  :plugins [[lein-cloverage "1.1.2"]]
+  :plugins [[lein-cloverage "1.2.0"]]
   :profiles { :dev {:dependencies [[metosin/malli "0.0.1-20200404.091302-14"]]}})
