7b7b43943a9556d0ad8256f44f8f3c6826e53761
Alexander Oloo <alekcz@gmail.com>
2020-10-06 08:15:10 +0200

    update konserve-fire version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [ [org.clojure/clojure "1.10.1" :scope "provided"]
-                  [alekcz/konserve-fire "0.3.0-20200919.161446-8"]
+                  [alekcz/konserve-fire "0.3.0-alpha1"]
                   [io.replikativ/datahike "0.3.2-20200918.094908-15" :exclusions [io.replikativ/superv.async]]]
   :aot :all                
   :repl-options {:init-ns datahike-firebase.core}
