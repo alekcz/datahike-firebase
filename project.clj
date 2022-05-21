05507ef99ecab9ea2bff44e88dd5dac47004b5af
Alex Oloo <Alex Oloo>
2022-05-21 08:48:13 +0200

    align datahike-firebase version to datahike version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alekcz/datahike-firebase "0.2.2"
+(defproject alekcz/datahike-firebase "0.4.1490"
   :description "Datahike with Firebase as data storage"
   :url "https://github.com/alekcz/datahike-firebase"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -7,8 +7,8 @@
                   [org.clojure/core.async "1.5.648"]
                   [environ "1.2.0"]
                   [alekcz/konserve-fire "0.4.0"]
-                  [io.replikativ/datahike "0.4.1480"]]
+                  [io.replikativ/datahike "0.4.1490"]]
   :aot :all     
   :main datahike-firebase.core            
   :repl-options {:init-ns datahike-firebase.core}
-  :plugins [[lein-cloverage "1.2.2"]])
+  :plugins [[lein-cloverage "1.2.3"]])
