edcaa3a9bc3861f99ce333349073b3ea82e2725c
alekcz <alekcz@gmail.com>
2022-08-12 11:39:51 +0200

    prep v0.5.1506

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alekcz/datahike-firebase "0.5.1504-SNAPSHOT"
+(defproject alekcz/datahike-firebase "0.5.1506-SNAPSHOT"
   :description "Datahike with Firebase as data storage"
   :url "https://github.com/alekcz/datahike-firebase"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -7,7 +7,7 @@
                   [org.clojure/core.async "1.5.648"]
                   [environ "1.2.0"]
                   [alekcz/konserve-fire "0.4.2-20220522.152957-1"]
-                  [io.replikativ/datahike "0.5.1504"]]
+                  [io.replikativ/datahike "0.5.1506"]]
   :aot :all     
   :main datahike-firebase.core            
   :repl-options {:init-ns datahike-firebase.core}
